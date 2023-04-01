e54e7c9c04423c15c7ef50de70942af83be76936
Igor Cerqueira Andrade <oigorwebs@gmail.com>
2023-04-01 11:56:32 -0300

    refactoring

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
-def maximo(c1,c2,cima,lado,diagonal):
-    if(c1 == c2 and (diagonal+1) >= cima and (diagonal+1) >= lado):
+def maximo(c1,c2,cima,esquerdo,diagonal):
+    if(c1 == c2 and (diagonal+1) >= cima and (diagonal+1) >= esquerdo):
         diagonal = diagonal+1
         return diagonal
-    elif (lado >= cima and lado >= diagonal):
-        return lado
+    elif (esquerdo >= cima and esquerdo >= diagonal):
+        return esquerdo
     else:
         return cima
     
-def ponteiro(c1, c2, cima, lado, diagonal):
-    if (c1 == c2 and (diagonal+1) >= cima and (diagonal+1) >= lado):
+def ponteiro(c1, c2, cima, esquerdo, diagonal):
+    if (c1 == c2 and (diagonal+1) >= cima and (diagonal+1) >= esquerdo):
         return '\\'
-    elif (lado >= cima and lado >= diagonal):
+    elif (esquerdo >= cima and esquerdo >= diagonal):
         return '_'
     else:
         return '|'
@@ -55,25 +55,41 @@ def lcs(v,w):
     ponteiros = []
     pontuacao = [0]*len(v)
     ponteiros = ['']*len(v)
-    
+
+    #inicializa matriz de pontos e ponteiros com zeros e espaços
     for i in range (0, len(w)):
         pontuacao[i] = [0]*len(v)
         ponteiros[i] = ['']*len(v)
-        
+
+    #inicializa a primeira coluna da matriz com barras
     for i in range (0, len(w)):
         ponteiros[i][0] = '|'
+
+    #inicializa a primeira linha da matriz com underscore
     for j in range (0, len(v)):
         ponteiros[0][j] = '_'
-        
-    for i in range (1, len(w)):
-        for j in range (1, len(v)):
-            pontuacao[i][j] = maximo(v[j],w[i], pontuacao[i-1][j], pontuacao[i][j-1], pontuacao[i-1][j-1])
-            ponteiros[i][j] = ponteiro(v[j],w[i], pontuacao[i-1][j], pontuacao[i][j-1], pontuacao[i-1][j-1])
+
+
+    for i in range (1, len(w)): # i = linha 
+        for j in range (1, len(v)): #j = coluna
+            cima = pontuacao[i-1][j]
+            esquerdo = pontuacao[i][j-1]
+            diagonal = pontuacao[i-1][j-1]
+            c1 = v[j]
+            c2 = w[i]
+            pontuacao[i][j] = maximo(c1,c2, cima, esquerdo, diagonal)
+            ponteiros[i][j] = ponteiro(c1,c2, cima, esquerdo, diagonal)
 
     imprimeMatriz(v, w, pontuacao, ponteiros)
     geraAlinhamento(v, w, pontuacao, ponteiros)
         
 #PROGRAMA PRINCIPAL:
-v = ['*', 'A', 'T', 'C', 'G', 'T', 'A', 'C']
-w = ['*', 'A', 'T', 'G', 'T', 'T', 'A', 'T']
+# v = ['*', 'A', 'T', 'C', 'G', 'T', 'A', 'C']
+# w = ['*', 'A', 'T', 'G', 'T', 'T', 'A', 'T']
+
+v = ['*', 'D', 'R', 'Q']
+w = ['*', 'D', 'R', 'Q']
+
+# v = ['*', "D","R","Q","T","A","Q","A","A","G","T","T","T","I","T"]
+# w = ['*', "D","R","N","T","A","Q","L","L","G","T","D","T","T"]
 lcs (v,w)  
